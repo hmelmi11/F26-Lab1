e271dd96cbbc1c078c52a1c5187004a94dd16b6a
hmelmi11 <helmi11@myseneca.ca>
2026-09-16 19:21:56 +0000

    completed Lab1a

--- lab1a.py
+++ lab1a.py
@@ -6,17 +6,21 @@
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: python3 lab1a.py
 
-# TO DO 1: Creating and using varibales
-# create a variable called message.
-# Set the variable to equal to "Welcome to PRG101".
-# Print the variable message using print() statement.
-
+message="Welcome to PRG101"
+print(message)
 # TO DO 2: Checking the type of a varibale
 # Use the builtin type() function and print the type of this variable.
-
+type(message)
 # TO DO 3: Dynamic Typing:
 # Create a varibel called `x` and assign it the value 10, then print the type of this variable.
+x=10
+print(type(x))
 
 # TO DO 4: Dynamic Typing: 
 # Now reassign a new value to the variable `x`, this value should be a string, e.g "hello", check the type of the variable `x` again.
 # What did you observe?
+x="Python is future"
+print(type(x))
+""" This is a multi
+line comment """
+
