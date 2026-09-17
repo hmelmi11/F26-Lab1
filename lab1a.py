dba2269979f50eded0af9c1ee108755dd86ebea2
hmelmi11 <helmi11@myseneca.ca>
2026-09-17 02:11:54 +0000

    Refactor lab1a.py to check variable types
    Removed TODO comments and added type checks for variable.

--- lab1a.py
+++ lab1a.py
@@ -8,19 +8,10 @@
 
 message="Welcome to PRG101"
 print(message)
-# TO DO 2: Checking the type of a varibale
-# Use the builtin type() function and print the type of this variable.
 type(message)
-# TO DO 3: Dynamic Typing:
-# Create a varibel called `x` and assign it the value 10, then print the type of this variable.
 x=10
 print(type(x))
-
-# TO DO 4: Dynamic Typing: 
-# Now reassign a new value to the variable `x`, this value should be a string, e.g "hello", check the type of the variable `x` again.
-# What did you observe?
 x="Python is future"
 print(type(x))
 """ This is a multi
 line comment """
-
