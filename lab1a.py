a7e4b4b73683281db4d2b72f4e25d70afe0fa512
hmelmi11 <helmi11@myseneca.ca>
2026-09-17 02:29:29 +0000

    Update lab1a.py

--- lab1a.py
+++ lab1a.py
@@ -1,17 +1,22 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author:Halima Elmi
+# Date:09/16/2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: python3 lab1a.py
 
 message="Welcome to PRG101"
 print(message)
+
 type(message)
+
 x=10
 print(type(x))
+
 x="Python is future"
+
 print(type(x))
+
 """ This is a multi
 line comment """
