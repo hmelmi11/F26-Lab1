29177978d0a74a6d02cb9fe62458945bc3cc170f
hmelmi11 <helmi11@myseneca.ca>
2026-09-17 02:12:31 +0000

    Correct variable assignments and arithmetic outputs
    Fixed variable assignment and print statements for arithmetic operations.

--- lab1b.py
+++ lab1b.py
@@ -6,20 +6,14 @@
 # Purpose: Use arithmetic in python.
 # Usage: python3 lab1b.py
 
-# TO-DO 1:
-#	Create a variable called "num1", take its value from user.
 num1=input("Please enter a num1")
-#	Create another variable called "num2" and take its value from user. 
 num2=input("Please enter another num for num2")
 print(type(num1))
 print(type (num2))
 num1=int(num1)
 num2int(num2)
-# Convert the values to integers using int() function
 num1=int(num1)
 num2=int(num2)
-# TO-DO 2:
-# Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
 print("Python")
 print("num1 + num2 =", (num1+num2))
 print("num1 - num2 =", (num1-num2))
